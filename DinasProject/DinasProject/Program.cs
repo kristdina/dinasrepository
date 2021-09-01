f2646b721008385010efcee0715ef2fdb29915fd
kristdina <kristdina@gmail.com>
2021-09-01 22:07:27 +0300

    my firts project hihihi

---
+++ DinasProject/DinasProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DinasProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
